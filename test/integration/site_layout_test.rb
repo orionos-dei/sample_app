b82d55858e91e07e4cb14832e3318fcecf0b1dc6
orionos-dei <orionos.dei@gmail.com>
2020-01-07 01:18:24 +0100

    Fix integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,8 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
     assert_select 'title', full_title("Home")
-    assert_select 'title', "Sign Up | #{@base_title}"
-
+    
   end
   
 end

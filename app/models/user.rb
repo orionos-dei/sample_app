36928d0b62bafed646fe2438cf947511dc002769
orionos-dei <orionos.dei@gmail.com>
2020-01-23 23:29:49 +0100

    Account Activation resources complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
-  before_save { self.email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name,  presence: true, length: {maximum:50}
@@ -35,4 +36,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
   
+  private
+  
+    def downcase_email
+      self.email = email.downcase
+    end
+  
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
